3be8e11f73e0f592d9046d92baaf96dc7e2d3cc7
Danilo3110 <lukic3@sbb.rs>
2019-08-03 23:27:25 +0200

    added new funcionality (Features), fixed some application bugs and refactoring code

--- src/main.js
+++ src/main.js
@@ -29,11 +29,46 @@ const render_Modal = () => {
                     </section>`);
     $('.video_overlay').prepend($modal);
     $('#x_icon').on('click', modalHandlerOff);
+    $('.modal_section .button, .modal_section .button-desc, #arrow_icon2').on('click', () => {
+        secondStepHandler(true);
+        modalHandlerOff();
+    });
     $('#email2').on('focus, input', () => enterEmail('#email2', '.modal_section'));
 };
 
-const secondStepHandler = () => {
-    if ($('#checkbox').is(':checked')) {
+const render_Features = () => {
+    const $features = $(`<section class="features">
+                            <div class="features_title position-absolute">Features</div>
+                            <div class="fast_block position-absolute">
+                                <img src="../assets/fast icon.png" alt="fast" class="fast_icon">
+                                <div class="info1">lorem ipsum</div>
+                                <div class="info2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore</div>
+                            </div>
+                            <div class="education_block position-absolute">
+                                <img src="../assets/education icon.png" alt="education" class="fast_icon icon1">
+                                <div class="info1">lorem ipsum</div>
+                                <div class="info2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore</div>
+                            </div>
+                            <div class="loophole_block position-absolute">
+                                <img src="../assets/loophole icon.png" alt="loophole" class="fast_icon icon2">
+                                <div class="info1">lorem ipsum</div>
+                                <div class="info2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore</div>
+                            </div>
+                            <div class="profit_block position-absolute">
+                                <img src="../assets/profit icon.png" alt="profit" class="fast_icon icon3">
+                                <div class="info1">lorem ipsum</div>
+                                <div class="info2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore</div>
+                            </div>
+                        </section>`);
+    $('.faq').prepend($features);
+};
+
+const secondStepHandler = (test) => {
+    if (test) {
         render_secondStep();
         $('.checkbox, .privacy').addClass('hide').fadeIn(0).fadeOut(300);
         $('.step_1').css('color', '#28b45d').fadeOut(0).fadeIn(500);
@@ -43,7 +78,7 @@ const secondStepHandler = () => {
         $('.data_protected').css({top: '562px'});
         $('.more_info').css({top: '611px'});
 
-        $('.button, #next-step').off('click');
+        $('.first_step .button, #next-step').off('click');
     } else {
         $('.privacy').css({borderBottom: '1px dashed #dc3545'});
     }
@@ -57,6 +92,21 @@ const modalHandlerOn = () => {
 const modalHandlerOff = () => {
     $('.modal_section').remove();
     $('.first_step').removeClass('hide');
+};
+
+const featuresHandlerOn = () => {
+    $('.faq').css({backgroundColor: 'unset'});
+    $('.faq_title, .questions').addClass('hide');
+    render_Features();
+    $('.steps_section .button, #features').off('click');
+    $('.steps_section .button, #features').on('click', featuresHandlerOff);
+};
+
+const featuresHandlerOff = () => {
+    $('.features').remove();
+    $('.faq').css({backgroundColor: '#28b45d'});
+    $('.faq_title, .questions').removeClass('hide');
+    $('.steps_section .button, #features').on('click', featuresHandlerOn);
 };
 
 const enterEmail = (input, hide) => {
@@ -75,10 +125,14 @@ const questionHandler = () => {
 
 const eventsAll = () => {
     $('#email').on('focus, input', () => enterEmail('#email', '.form'));
-    $('.button, #next-step').on('click', secondStepHandler);
+    $('.first_step .button, #next-step').on('click', () => {
+        const test = $('#checkbox').is(':checked');
+        secondStepHandler(test);
+    });
     $('.data_protected div').on('click', () => $('.more_info').fadeToggle('hide'));
     $('.question1 .icon').on('click', questionHandler);
     $('.here').on('click', modalHandlerOn);
+    $('.steps_section .button, #features').on('click', featuresHandlerOn);
 };
 
 $(document).on('load', eventsAll());
